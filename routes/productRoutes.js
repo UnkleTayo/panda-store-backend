f5b075c4ea48315ff352a56ce7a5e896628711af
Adetayo <akinsanyaadetayo7@gmail.com>
2021-02-15 23:18:04 +0100

    FEAT: Version Api

---
+++ routes/productRoutes.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import asyncHandler from 'express-async-handler';
+import Product from '../Model/productModel.js';
+const router = express.Router();
+
+router.get(
+  '/',
+  asyncHandler(async (req, res) => {
+    const products = await Product.find({});
+
+    // @desc Fetch all products
+    // @route GET /api/v1/products
+    //  @access Public
+    res.status(200).json(products);
+  })
+);
+
+// @desc Fetch single product
+// @route GET /api/v1/products/:id
+//  @access Public
+router.get(
+  '/:id',
+  asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404).json({ message: 'Product not found' });
+    }
+  })
+);
+
+export default router;
