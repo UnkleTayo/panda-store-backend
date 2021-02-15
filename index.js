d18bfe9b006f95ef59a6320f188d041d8cae5767
Adetayo <akinsanyaadetayo7@gmail.com>
2021-02-16 00:03:28 +0100

    CHORE: Create error handler route

--- index.js
+++ index.js
@@ -1,29 +1,24 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
+
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
-import products from './data/products.js';
-
+import { errorHandler, notFound } from './middleware/errorMiddleware.js';
 dotenv.config();
 
 connectDB();
 const app = express();
 
-// Error middleware
-app.use((err, req, res, next) => {
-  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-  res.status(statusCode);
-  res.json({
-    message: err.message,
-    stack: process.env.NODE_ENV === 'production' ? null : err.stack,
-  });
-});
 app.get('/', (req, res) => {
-  res.send('Api is running');
+  res.send('Welcome ro Panda Shop');
 });
 
 app.use('/api/v1/products', productRoutes);
+
+// Error middleware
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
