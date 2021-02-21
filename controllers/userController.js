fc15025a9bb7cd317b34e5d9014039c27a29dc74
Adetayo <akinsanyaadetayo7@gmail.com>
2021-02-21 02:53:29 +0100

    Authenticate user login

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,6 +10,21 @@ import User from '../Model/userModel.js';
 
 const authUser = asyncHandler(async (req, res) => {
  const {email, password} = req.body
+
+ const user = await User.findOne({email})
+
+ if (user && (await user.matchPassword(password))){
+   res.json({
+     _id: user._id,
+     name: user.name,
+     email: user.email,
+     isAdmin: user.isAdmin,
+     token: null
+   })
+ } else{
+   res.status(401)
+   throw new Error("Invalid email or password")
+ }
   res.send({email, password})
 })
 
