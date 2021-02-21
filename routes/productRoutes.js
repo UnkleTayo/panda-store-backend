4e9ac70b54c2d5f189887e6be42b297c129556c0
Adetayo <akinsanyaadetayo7@gmail.com>
2021-02-21 02:22:46 +0100

    Chore: Implement controller for products

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,7 +2,7 @@ import express from 'express';
 const router = express.Router();
 import { getProductbyId, getProducts } from '../controllers/productController.js'
 
-router.route('/'.get(getProducts))
-router.route('/:id'.get(getProductbyId))
+router.route('/').get(getProducts)
+router.route('/:id').get(getProductbyId)
 
 export default router;
