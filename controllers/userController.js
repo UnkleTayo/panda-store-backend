08a7f8a4797e46a062e6f2febdf688d14b08106c
Adetayo <akinsanyaadetayo7@gmail.com>
2021-03-23 23:48:26 +0100

    Update user functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -91,6 +91,39 @@ const registerUser = asyncHandler(async (req, res) => {
 
 })
 
+
+
+/*
+@desc Update User Profile
+@route PUT /api/v1/users/profile
+@access private
+*/  
+const updateUserProfile  = asyncHandler(async (req, res) => { 
+
+  const user = await User.findById(req.user._id)
+   
+    if (user){
+      user.name = req.body.name || user.name
+      user.email = req.body.email || user.email
+      if (req.body.password){
+        user.password = req.body.password
+      }
+      const updatedUser = await res.save()
+      res.json({
+        _id: updatedUser._id, 
+        name: updatedUser.name,
+        email: updatedUser.email,
+        isAdmin: updatedUser.isAdmin,
+        token: generateToken(updatedUser._id)
+      })
+       } else{
+      res.status(401)
+      throw new Error("Invalid email or password")
+    }
+     res.send({email, password,})
+   })
+  
+
  
 
-export {authUser,getUserProfile, registerUser}
+export {authUser,getUserProfile, registerUser, updateUserProfile}
